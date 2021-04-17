4d5e7ddd28dcc15a79844e4c01272ae0bb617908
C <earthhominid@gmail.com>
2021-04-17 13:04:25 -0300

    added route

--- app.py
+++ app.py
@@ -6,4 +6,10 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
